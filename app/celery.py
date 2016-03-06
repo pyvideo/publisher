d2fd04cf24038f9637b0d828b1c1eb4e66a878a3
Paul Logston <code@logston.me>
2016-03-06 23:22:39 +0000

    Build task to publish media record

--- app/celery.py
+++ app/celery.py
@@ -1,8 +1,18 @@
+import hashlib
+import json
+import os
+import shutil
+import subprocess
+
 import celery as celery_lib
 import raven
 from raven.contrib.celery import register_signal, register_logger_signal
 
 from . import app
+
+
+WORKING_DIR = '/tmp/pytube'
+REPO_URL = 'git@github.com:pytube/pytube.git'
 
 
 sentry_dsn = app.config.get('SENTRY_DSN')
@@ -37,31 +47,64 @@ def make_celery(app):
 celery = make_celery(app)
 
 
+def subproc(args, timeout=10):
+    ret = subprocess.Popen(args, cwd=WORKING_DIR).wait(timeout=timeout)
+    if ret:
+        raise ValueError('Got retrun code {} from executing {}'.format(ret, args))
+
+
 @celery.task
 def publish_media_record(data_file):
     """
     Publish a media request.
     """
-    print(locals())
-    # check working area is clean
+    # ensure working area is set
+    if os.path.exists(os.path.join(WORKING_DIR, '.git')): 
+        subproc(('/usr/bin/git', 'checkout', 'master'))
+        subproc(('/usr/bin/git', 'reset', 'HEAD', '--hard'))
+        subproc(('/usr/bin/git', 'pull', 'origin', 'master'), timeout=30)
 
-    # clone repo
+    else:
+        subproc(('/usr/bin/git', 'clone', REPO_URL), timeout=60)
+
+    # create new branch
+    m = hashlib.md5()
+    m.update(data_file.encode())
+    digest = m.hexdigest()
+
+    subproc(('/usr/bin/git', 'checkout', '-b', digest))
 
     # find file in data
+    abs_data_file_path = os.path.join(WORKING_DIR, data_file)
+    if not os.path.exists(abs_data_file_path):
+        raise RuntimeError('No file at path: {}'.format(abs_data_file_path))
 
     # de-serialize file
-
-    # find media record slug
-
+    with open(abs_data_file_path) as fp:
+        data = json.load(fp)
+ 
     # check that talk is not already published
+    if data['status'] == 'published':
+        return
 
     # update media record file with status: published
+    data['status'] = 'published'
 
-    # make new branch
+    with open(abs_data_file_path, 'w') as fp:
+        json.dump(data, fp)
+
+    subproc(('/usr/bin/git', 'add', data_file))
+
+    # commit changes
+    subproc(('/usr/bin/git', 'commit', '-m', 'Publish {}'.format(digest)))
 
     # merge new branch into master
+    subproc(('/usr/bin/git', 'checkout', 'master'))
+    subproc(('/usr/bin/git', 'merge', digest))
+
+    # delete feature branch
+    subproc(('/usr/bin/git', 'branch', '-D', digest))
 
     # push to master
-
-    # clean up working area
+    subproc(('/usr/bin/git', 'push', 'origin', 'master'))
 
